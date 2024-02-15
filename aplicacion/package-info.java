05577a487090e3aebc0a4fa866b34da0c7a5dfb5
alejandravazcor <145661135+alejandravazcor@users.noreply.github.com>
2024-02-15 11:45:19 +0100

    Update package-info.java

--- aplicacion/package-info.java
+++ aplicacion/package-info.java
@@ -1,1 +1,11 @@
 package aplicacion; 
+/**
+ * Este paquete contiene la clase Principal, que es la clase de entrada
+ * principal para la aplicación. La clase proporciona un método main que
+ * acepta un argumento de línea de comandos que representa el número de pasos
+ * a utilizar en el cálculo de la aproximación al número Pi mediante el
+ * método de Montecarlo.
+ * Utiliza la clase Matematicas del paquete mates para realizar el
+ * cálculo y mostrar el resultado.
+ */
+
