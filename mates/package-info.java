7ae2093a84984d0bb6c4eed5d3cc9bca84efd8ab
alejandravazcor <145661135+alejandravazcor@users.noreply.github.com>
2024-02-15 11:43:35 +0100

    Update package-info.java

--- mates/package-info.java
+++ mates/package-info.java
@@ -4,8 +4,4 @@ package mates;
  * Incluye la clase Matematicas que proporciona métodos para realizar
  * cálculos matemáticos, como la generación de una aproximación al número Pi
  * mediante el método de Montecarlo.
- *
- * @author Alejandra
- * @version 1.0
- * @since 2024-02-15
  */
