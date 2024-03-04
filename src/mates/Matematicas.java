18f68c255525852cc94efc537aa75708894cc58e
alejandravazcor <145661135+alejandravazcor@users.noreply.github.com>
2024-03-04 11:11:24 +0100

    Update Matematicas.java

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -6,21 +6,22 @@ import java.util.random.RandomGenerator;
 
 public class Matematicas {
     /**
-     * * Genera una aproximación al número Pi mediante el método de
-     * * Montecarlo. El parámetro `pasos` indica el número de puntos
-     * * generado.
-     * * i=contador de lanzamientos lso lanzamientos inicialmente son 0
-     * * dardos Disponibles (ARGS (O) ) es el primer parametro que introduzco por consola (hay que usar integerparseint)
-     * * f¿EL numero de lanzamientos (int) es menor  a los dardos que tengo disponibles?
-     * * si es SI es menor o igual entonces tengo que lanzar dardos luego me pregunto ¿El dardo ha caido dentro?
-     * * si la respuesta es si, entonces hago acierto es aciertos+1 incremento y tambien tengo que incrementar los dardos que he lanzado
-     * * tengo que generar dos numeros aleatorios entre el 0 y 1 (random) y eso es el lanzar dardos ¿ como se generan numero s aleatorios en java?
-     * * ¿Como se si el dardo ha caido dentro si el centro esta en o,5 , 0,5  tengo que coger el si me sale menor o igual que el radio es que me ha caido dentro y si me sale mayor o igual qeu el radio es que me ha caido fuera
-     * * la programacions del segundo rombo es calcular el radio y luego pasar al sigueinte paso
+     * Genera una estimación de la constante matemática Pi utilizando un método recursivo.
+     *
+     * @param dardosDisponibles El número de dardos disponibles para la estimación.
+     * @return Una estimación de Pi basada en el método de Monte Carlo.
      */
     public static double generarNumeroPiRecursivo(long dardosDisponibles) {
         return generarNumeroPiRecursivoAux(dardosDisponibles, 0, 0);
     }
+	/**
+     * Método auxiliar para generar Pi de manera recursiva utilizando el método de Monte Carlo.
+     *
+     * @param dardosDisponibles El total de dardos disponibles para la estimación.
+     * @param lanzamientos      El número actual de lanzamientos de dardos.
+     * @param aciertos          El número actual de aciertos de dardos dentro del círculo unitario.
+     * @return Una estimación de Pi basada en el método de Monte Carlo.
+     */
 
     public static double generarNumeroPiRecursivoAux(long dardosDisponibles, long lanzamientos, long aciertos) {
         if (lanzamientos < dardosDisponibles){ 
