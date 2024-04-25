1eeb6b81f5fc663855a7d53d465d947292ba0ec9
Alejandra Vazquez <alejandra.vazquezcorbella@usp.ceu.es>
2024-04-25 11:34:50 +0200

    Eliminado el directorio mates y el archivo Matematicas.java

--- src/mates/package-info.java
+++
@@ -1,7 +0,0 @@
-package mates; 
-/**
- * Este paquete contiene clases relacionadas con operaciones matemáticas.
- * Incluye la clase Matematicas que proporciona métodos para realizar
- * cálculos matemáticos, como la generación de una aproximación al número Pi
- * mediante el método de Montecarlo.
- */
