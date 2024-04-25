4e9dc731ffc799d856c6eec491fd919fd6fdaeb3
Alejandra Vazquez <alejandra.vazquezcorbella@usp.ceu.es>
2024-04-25 11:47:45 +0200

    Primera version practica 4 sin errores de compliacion

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,9 +2,18 @@ package aplicacion;
 
 import palabras.CalculadoraDistancias;
 
-public class Principal{
-    public static void main(String[] args){
-        System.out.println("La distancia entre las palabras " + args[0] + " y " + args[1] + " es " + 
-                            CalculadoraDistancias.calcularDistancia(args[0], args[1]));
+public class Principal {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Se requieren al menos dos palabras como argumentos.");
+            return; // Salir del programa si no hay suficientes argumentos
+        }
+
+        String palabra1 = args[0];
+        String palabra2 = args[1];
+
+        System.out.println("La distancia entre las palabras " + palabra1 + " y " + palabra2 + " es " +
+                CalculadoraDistancias.calcularDistancia(palabra1, palabra2));
     }
-}}
+}
+
